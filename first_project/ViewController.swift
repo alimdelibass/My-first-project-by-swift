3604a32f86241e4135ad49790b9c744af40b6a5d
Alim delibas <alim.delibass@gmail.com>
2023-12-04 23:37:45 +0300

    Initial Commit

---
+++ first_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_project
+//
+//  Created by alim on 4.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
